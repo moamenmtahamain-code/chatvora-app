ceb43e3ed808ef8b0a4ceb8101f6d768e076ac9c
moamenmtahamain-code <moamen.m.tahamain@gmail.com>
2026-06-02 23:22:17 +0300

    Fix routing to allow chat access

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"wBGXw17BV/QfXJIc/FFOsBeQaxOxIKDEU/lpYQKCKKM=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OKnxsXrt2LavrYDHuzSsOgDYHAug9gM32q4at1QxlqI=\"\n}"
